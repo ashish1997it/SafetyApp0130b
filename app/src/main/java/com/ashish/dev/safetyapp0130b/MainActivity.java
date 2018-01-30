cef790491280dcdcdf4b89d78b235e27e7366f57
ashish1997it <sondagarashish@gmail.com>
2018-01-30 15:25:03 +0530

    Store Data

--- app/src/main/java/com/ashish/dev/safetyapp0130b/MainActivity.java
+++ app/src/main/java/com/ashish/dev/safetyapp0130b/MainActivity.java
@@ -1,19 +1,26 @@
 package com.ashish.dev.safetyapp0130b;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFirebaseBtn;
     private DatabaseReference mDatabase;
     private EditText mNameField;
+    private EditText mEmailField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mNameField = (EditText) findViewById(R.id.name_field);
+        mEmailField = (EditText) findViewById(R.id.email_field);
 
         mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +40,22 @@ public class MainActivity extends AppCompatActivity {
                 //1- Create child in root object
                 //2- Assign some value to the child object tut8
                 String name = mNameField.getText().toString().trim();
-                mDatabase.child("Name").setValue(name);
+                String email = mEmailField.getText().toString().trim();
+
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name",name);
+                dataMap.put("Email",email);
+
+                //check firebase task complited or not
+                mDatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Stored :)",Toast.LENGTH_LONG).show();
+                        }else
+                            Toast.makeText(MainActivity.this,"Not Stored :(",Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
     }
